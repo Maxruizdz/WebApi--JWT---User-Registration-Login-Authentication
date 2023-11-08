29f9bc6c806176f1a5698bbe9c802f99662ac70b
Maxruizdz <maxruizdz@hotmail.com>
2023-11-08 17:16:10 -0300

    Primer commit- Creacion de models y controladores

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Api_CreateJWT_User_Regis_Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
---
+++ Models/UserDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Web_Api_CreateJWT_User_Regis_Authentication.Models
+{
+    public class UserDto
+    {
+    }
+}
