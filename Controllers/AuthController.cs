d203ea4fd5ada21b169e9e4feae460bd726696aa
Maxruizdz <maxruizdz@hotmail.com>
2023-11-08 17:38:57 -0300

    Creacion del endpoint Login en AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BCrypt.Net;
+
+using Web_Api_CreateJWT_User_Regis_Authentication.Models;
 
 namespace Web_Api_CreateJWT_User_Regis_Authentication.Controllers
 {
@@ -7,5 +10,47 @@ namespace Web_Api_CreateJWT_User_Regis_Authentication.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public static User user = new User();
+        [HttpPost("Register")]
+        public ActionResult<User> Register(UserDto request) {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.PasswordHash);
+
+            user.Username = request.Username;
+            user.Password = passwordHash;
+        
+        return new JsonResult(user);
+        
+         }
+
+        [HttpPost("Login")]
+        public ActionResult<User> Login(UserDto request ) {
+
+
+            if (user.Username!=request.Username) {
+
+                return BadRequest("User not found");
+            
+            
+            
+           }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.PasswordHash, user.Password)) {
+
+
+
+                return BadRequest("Wrong password.");
+            
+            
+            }        
+
+
+            return Ok(user);
+        
+        
+        }
+
+
+
+
     }
 }
